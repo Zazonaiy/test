85cfcb6a4a4eacb45a3694dc4273c8b95a2e3639
gengxiaoxiao <gengxiaoxiao@3gwin.cn>
2020-04-29 12:24:44 +0800

    init

---
+++ hello.js
@@ -0,0 +1,10 @@
+
+console.log("hello world");
+
+console.log("this is line 1");
+console.log("this is line 2");
+console.log("this is line 3");
+console.log("this is line 4");
+console.log("this is line 5");
+
+console.log("finished");
